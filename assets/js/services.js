c005ce1065962c1f6784ef760f9b332f00182026
dra1n <dra1n86@gmail.com>
2015-02-14 09:15:15 +0200

    Added homework results.

--- assets/js/services.js
+++ assets/js/services.js
@@ -1,5 +1,9 @@
 angular.module('jsLessons')
 
-.factory('StudentsResults', ['loadBuild', function(loadBuild) {
-  return 'hey';
+.factory('StudentsResults', ['$http', 'HOMEWORKS', function($http, HOMEWORKS) {
+  var repos = Object.keys(HOMEWORKS).map(function(topic) {
+    return HOMEWORKS[topic];
+  });
+
+  return $http.jsonp('http://homework-results.herokuapp.com/results?repos='+repos+'&callback=JSON_CALLBACK');
 }]);
